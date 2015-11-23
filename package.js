7201cc21ab75e73eacfc898815d013330e967c3f
Jimmy Aupperlee <j.aup.gt@gmail.com>
2015-11-23 14:55:04 +0100

    Alright, we don'

--- package.js
+++ package.js
@@ -13,7 +13,7 @@ Cordova.depends({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3.0');
+  api.versionsFrom('1.2.1');
   api.use('ecmascript');
   api.addFiles(
     'client/event.js',
